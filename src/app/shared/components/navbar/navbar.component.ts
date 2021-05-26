d9086c8f6f41bfe7c648b216a7824efd8f1bf8ec
Osgiliath <ManuelGlez>
2021-05-26 09:23:37 -0600

    navbar scroll style and scroll-top button

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalConstants } from 'src/app/common/global-constants';
+import { HostListener } from '@angular/core';
+import { faBars, faHamburger } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -9,12 +11,25 @@ import { GlobalConstants } from 'src/app/common/global-constants';
 export class NavbarComponent implements OnInit {
   gc = GlobalConstants;
   toggleOpen: boolean;
+  icons={
+    menu: faBars
+  }
   constructor() { }
 
   ngOnInit(): void {
+
   }
   toggleMenu() {
     this.toggleOpen = !this.toggleOpen;
+  }
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let element = document.querySelector('.navbar') as HTMLElement;
+    if (window.pageYOffset < element.clientHeight) {
+      element.classList.add('navbar-inverse');
+    } else {
+      element.classList.remove('navbar-inverse');
+    }
   }
 
 }
